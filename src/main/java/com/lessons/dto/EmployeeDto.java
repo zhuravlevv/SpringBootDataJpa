954e3d6f4505561acf0cbc4742c873e24c85b6a4
Vlad Zhuravlev <vlad12.09.01.1@mail.ru>
2020-07-22 03:32:27 +0300

    add dto

---
+++ src/main/java/com/lessons/dto/EmployeeDto.java
@@ -0,0 +1,18 @@
+package com.lessons.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeDto {
+
+    private Integer id;
+    private String name;
+    private Integer departmentId;
+
+}
