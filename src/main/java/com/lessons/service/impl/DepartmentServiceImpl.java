5dbd86a388a50365c760414ca0c00467ed5eae34
Vlad Zhuravlev <vlad12.09.01.1@mail.ru>
2020-07-26 23:27:54 +0300

    add LOGGER to service

--- src/main/java/com/lessons/service/impl/DepartmentServiceImpl.java
+++ src/main/java/com/lessons/service/impl/DepartmentServiceImpl.java
@@ -6,6 +6,8 @@ import com.lessons.entity.Department;
 import com.lessons.service.DepartmentService;
 import com.lessons.service.mapper.DepartmentMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +22,11 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     private final DepartmentMapper departmentMapper;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentServiceImpl.class);
+
     @Override
     public List<DepartmentDto> getAll() {
+        LOGGER.trace("getAll()");
         return departmentDao.findAll()
                 .stream()
                 .map(departmentMapper::fromEntity)
@@ -30,24 +35,27 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     @Override
     public DepartmentDto getById(Integer id) {
+        LOGGER.trace("getById({})", id);
         try {
             return departmentMapper.fromEntity(departmentDao
                     .findById(id)
                     .orElseThrow(Exception::new));
         }
         catch (Exception e){
-            System.out.println("Department with id = " + id + " doesn't exist.");
+            LOGGER.error("Department with id = {} doesn't exist.", id);
         }
         return null;
     }
 
     @Override
     public DepartmentDto save(DepartmentDto departmentDto) {
+        LOGGER.trace("save({})", departmentDto);
         return departmentMapper.fromEntity(departmentDao.save(departmentMapper.toEntity(departmentDto)));
     }
 
     @Override
     public DepartmentDto update(DepartmentDto newDepartment, Integer id) {
+        LOGGER.trace("update({}, {})", newDepartment, id);
         try {
             Department department = departmentDao.findById(id).orElseThrow(Exception::new);
             department.setName(newDepartment.getName());
@@ -55,13 +63,15 @@ public class DepartmentServiceImpl implements DepartmentService {
             return departmentMapper.fromEntity(department);
         }
         catch (Exception e){
-            System.out.println("Department with id = " + id + " doesn't exist.");
+            LOGGER.error("Department with id = {} doesn't exist.", id);
+            System.out.println();
         }
         return null;
     }
 
     @Override
     public void delete(Integer id) {
+        LOGGER.trace("delete({})", id);
         departmentDao.deleteById(id);
     }
 }
--- src/main/java/com/lessons/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/lessons/service/impl/EmployeeServiceImpl.java
@@ -8,6 +8,8 @@ import com.lessons.entity.Employee;
 import com.lessons.service.EmployeeService;
 import com.lessons.service.mapper.EmployeeMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,9 +27,11 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeMapper employeeMapper;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     @Override
     public List<EmployeeDto> getAll() {
+        LOGGER.trace("getAll()");
         return employeeDao.findAll()
                 .stream()
                 .map(employeeMapper::fromEntity)
@@ -36,19 +40,21 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public EmployeeDto getById(Integer id) {
+        LOGGER.trace("getById({})", id);
         try {
             return employeeMapper.fromEntity(employeeDao
                     .findById(id)
                     .orElseThrow(Exception::new));
         }
         catch (Exception e){
-            System.out.println("Employee with id = " + id + " doesn't exist.");
+            LOGGER.error("Employee with id = {} doesn't exist.", id);
         }
         return null;
     }
 
     @Override
     public EmployeeDto save(EmployeeDto employeeDto) {
+        LOGGER.trace("save({})", employeeDto);
         return employeeMapper.fromEntity(employeeDao
                 .save(employeeMapper
                         .toEntity(employeeDto)));
@@ -56,6 +62,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public EmployeeDto update(EmployeeDto newEmployee, Integer id) {
+        LOGGER.trace("update({}, {})", newEmployee, id);
         try {
             Employee employee = employeeDao
                     .findById(id)
@@ -69,24 +76,26 @@ public class EmployeeServiceImpl implements EmployeeService {
             return employeeMapper.fromEntity(employee);
         }
         catch (Exception e){
-            System.out.println("Employee with id = " + id + " doesn't exist.");
+            LOGGER.error("Employee with id = {} doesn't exist.", id);
         }
         return null;
     }
 
     @Override
     public void delete(Integer id) {
+        LOGGER.trace("delete({})", id);
         employeeDao.deleteById(id);
     }
 
     @Override
     @Transactional
     public void deleteByDepartmentId(Integer departmentId) {
+        LOGGER.trace("deleteByDepartmentId({})", departmentId);
         try {
             Department department = departmentDao.findById(departmentId).orElseThrow(Exception::new);
             employeeDao.deleteAllByDepartment(department);
         } catch (Exception e){
-            System.out.println("Department with id = " + departmentId + " doesn't exist.");
+            LOGGER.error("Department with id = {} doesn't exist.", departmentId);
         }
     }
 }
